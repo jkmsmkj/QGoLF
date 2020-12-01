33e55bed55a4a822365384fca01945d9b12fbaff
Dharmesh Jain <jkmsmkj@outlook.com>
2020-12-01 16:35:37 +0530

    Updated Android Resources

--- QGoLF.Android/MainActivity.cs
+++ QGoLF.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace QGoLF.Droid
 {
-    [Activity(Label = "QGoLF", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "QGoLF", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
